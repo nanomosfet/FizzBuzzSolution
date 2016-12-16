87398fc21b65b2a9c7b98edf0c9c53b2f27527aa
nanomosfet <timmyzsearcy@gmail.com>
2016-12-16 13:48:16 -0800

    Interview question to create a Stack Class

---
+++ ourStack.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class ourStack {
+	
+	private int[] ourList = new int[10];
+	
+	private int StackPointer = 0;
+	
+	public void push(int num) {
+		if(StackPointer >= 10)
+			throw new IndexOutOfBoundsException();
+		ourList[StackPointer] = num;
+		StackPointer++;
+		
+	}
+	
+	public int pop() {
+		int res = ourList[StackPointer - 1];
+		
+		StackPointer--;
+		return res;
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		ourStack myStack = new ourStack();
+		for (int i = 0; i < 10; i++ )
+			myStack.push(i);
+		System.out.print(myStack.pop());
+		
+	}
+	
+	
+}
+
